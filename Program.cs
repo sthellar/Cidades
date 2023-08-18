9146a15739fbb3b50c567555e51b1cbf3c1470bb
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-08-18 11:53:05 -0300

    Adicina soluçãodo exercício Cidades

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+
+
